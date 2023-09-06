ff333fecaf404a803221f817ea776d783c829c97
Denys <denys.hudymov@gmail.com>
2023-09-06 16:01:52 +0300

    Commented code was deleted

--- lab1/main.py
+++ lab1/main.py
@@ -9,17 +9,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-# def open_file():
-#     file_path = filedialog.askopenfilename()
-#     if file_path:
-#         selected_file_label.config(text=f"Selected File: {file_path}")
-#
-# root = tk.Tk()
-# root.title("File Selection Example")
-#
-# browse_button = tk.Button(root, text="Browse", command=open_file)
-# browse_button.pack(pady=20)
-#
-# selected_file_label = tk.Label(root, text="Selected File: None")
-# selected_file_label.pack()
-
