f9231e721c28903b9226dcfb5a90e9a226464930
Denys <denys.hudymov@gmail.com>
2023-09-10 02:00:15 +0300

    was added a method for creating distribution frequency table

--- lab1/Controller.py
+++ lab1/Controller.py
@@ -1,9 +1,19 @@
 import tkinter as tk
 import numpy as np
 from FrequencyTable import FrequencyTable
+from FrequencyDistributionTable import FrequencyDistributionTable
 
 def f_table(data):
         __table = FrequencyTable(data)
         if not (__table is None or np.array_equiv(__table.get_data(), data)):
                 __table.set_data(data)
         __table.display()
+
+def f_distribution_table(data,number_of_class = -1):
+        __dist_table = FrequencyDistributionTable(data)
+        if not (__dist_table is None or np.array_equiv(__dist_table.get_data(), data)):
+                __dist_table.set_data(data)
+                __dist_table.set_number_of_classes(__dist_table.find_number_of_classes(len(data)))
+        if number_of_class != -1:
+                __dist_table.set_number_of_classes(number_of_class)
+        __dist_table.display()
