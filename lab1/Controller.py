fb0b792c60ff39bac0e1f17cd6cc74b9749c6f11
Denys <denys.hudymov@gmail.com>
2023-09-13 21:18:58 +0300

    Controller.py file was refactored there was created a class Controller that does the same logic as before

--- lab1/Controller.py
+++ lab1/Controller.py
@@ -3,40 +3,22 @@ import numpy as np
 from tkinter import ttk
 from FrequencyTable import FrequencyTable
 from FrequencyDistributionTable import FrequencyDistributionTable
-from Histogram import BarPlot
 
-def create_table_tab(data):
-        create_frequency_table(data)
-        create_frequency_distribution_table(data)
 
-def create_frequency_table(data):
+class Controller:
+    def create_table_tab(self, data):
+        self.create_frequency_table(data)
+        self.create_frequency_distribution_table(data)
+
+    def create_frequency_table(self, data):
         __table = FrequencyTable(data)
         if not (__table is None or np.array_equiv(__table.get_data(), data)):
-                __table.set_data(data)
+            __table.set_data(data)
         __table.display()
 
-def create_frequency_distribution_table(data):
+    def create_frequency_distribution_table(self, data):
         __dist_table = FrequencyDistributionTable(data)
         if not (__dist_table is None or np.array_equiv(__dist_table.get_data(), data)):
-                __dist_table.set_data(data)
-                __dist_table.set_number_of_classes(__dist_table.find_number_of_classes(len(data)))
+            __dist_table.set_data(data)
+            __dist_table.set_number_of_classes(__dist_table.find_number_of_classes(len(data)))
         __dist_table.display()
-        create_histogram([__dist_table.get_dictionary().get("Class Width"),
-                          __dist_table.get_dictionary().get("Relative Frequency")])
-
-def create_histogram(data):
-
-        root = tk.Tk()
-        root.title("Plot Controller")
-
-        container = ttk.Frame(root)
-        container.pack(fill=tk.BOTH, expand=True)
-
-        frame = ttk.Frame(container)
-        frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        #разобраться что такое frame and how to display controller window
-
-        plot = BarPlot(frame, "Histogram", data)
-        plot.create_plot(frame)
-        root.deiconify()
-
