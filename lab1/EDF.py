cdc26a3ca89d983bcf741cfc9ec2a6e3efa7b76d
Denys <denys.hudymov@gmail.com>
2023-09-14 21:16:04 +0300

    Was created a class that create empirical distribution function

---
+++ lab1/EDF.py
@@ -0,0 +1,32 @@
+import numpy as np
+import tkinter as tk
+
+import matplotlib.pyplot as plt
+import numpy as np
+from Plot import BasePlot
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
+
+
+class EmpiricalDistributionPlot(BasePlot):
+    def __init__(self, frame, title, data):
+        super().__init__(frame, title, data)
+
+    def plot(self):
+        self.ax.step(self.data[0] , self.data[1], where="post")
+        self.ax.set_title(self.title)
+        self.figure.set_dpi(85)
+        self.figure.set_size_inches(5, 5)
+        plt.xlabel('Data Values')
+        plt.ylabel('Empirical Distribution Function (EDF)')
+        plt.grid(True)
+
+    def create_plot(self,canvas):
+        plt.tight_layout()
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        toolbar = NavigationToolbar2Tk(canvas, self.frame)
+        toolbar.update()
+        canvas_widget.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    def get_frame(self):
+        return self.frame
