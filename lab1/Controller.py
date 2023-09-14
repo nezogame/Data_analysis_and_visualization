12fd7823c4d3ad57b7c887558224c60e26ce7dae
Denys <denys.hudymov@gmail.com>
2023-09-14 21:14:13 +0300

    Was added an instance of Graph that shared in the constructor by all tables

--- lab1/Controller.py
+++ lab1/Controller.py
@@ -3,21 +3,24 @@ import numpy as np
 from tkinter import ttk
 from FrequencyTable import FrequencyTable
 from FrequencyDistributionTable import FrequencyDistributionTable
-
+from Graph import Graph
 
 class Controller:
+    def __init__(self, root):
+        self.__graph_table = Graph(root)
+
     def create_table_tab(self, data):
         self.create_frequency_table(data)
         self.create_frequency_distribution_table(data)
 
     def create_frequency_table(self, data):
-        __table = FrequencyTable(data)
+        __table = FrequencyTable(data, self.__graph_table)
         if not (__table is None or np.array_equiv(__table.get_data(), data)):
             __table.set_data(data)
         __table.display()
 
     def create_frequency_distribution_table(self, data):
-        __dist_table = FrequencyDistributionTable(data)
+        __dist_table = FrequencyDistributionTable(data, self.__graph_table)
         if not (__dist_table is None or np.array_equiv(__dist_table.get_data(), data)):
             __dist_table.set_data(data)
             __dist_table.set_number_of_classes(__dist_table.find_number_of_classes(len(data)))
