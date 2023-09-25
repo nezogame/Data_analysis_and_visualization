b697744521baa9ffb7642045ab795540c4924f0f
Denys <denys.hudymov@gmail.com>
2023-09-25 04:10:49 +0300

    now data from file is sorted on the start

--- lab1/FileReader.py
+++ lab1/FileReader.py
@@ -1,21 +1,23 @@
 import numpy as np
 import pandas as pd
 import dataclasses as dataclass
+from collections import OrderedDict
+
 
 class FileReader:
-    def __init__(self,file_path):
-        if not file_path.lower().endswith(('.dat','.txt')):
+    def __init__(self, file_path):
+        if not file_path.lower().endswith(('.dat', '.txt')):
             raise NameError("File must be a '.dat' or '.txt' extension")
         self.path = file_path
         self.__file_data = self.open_file(self.path)
 
-    def open_file(self,path):
-        data=[]
-        with open(path,'r') as file_reader:
+    def open_file(self, path):
+        data = []
+        with open(path, 'r') as file_reader:
             for line in file_reader:
                 for num in line.split():
                     data.append(float(num))
-            return np.array(data,dtype=float)
+            return np.sort(np.array(data, dtype=float))
         #     return [float(num) for line in file for num in line.split()]
 
     def get_file_object(self):
